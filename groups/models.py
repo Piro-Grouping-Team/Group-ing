e64b5c0e58ffcb0e3558621e1d79bb503072c775
doongdang <whtjrgus0428@gmail.com>
2022-08-05 02:56:43 +0900

    feat: create 구현

--- groups/models.py
+++ groups/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from logins.models import User
 
 # Create your models here.
 class Group(models.Model):
@@ -8,12 +9,12 @@ class Group(models.Model):
     introduction = models.TextField(null=True, blank=True, verbose_name='그룹 소개')
     purpose = models.TextField(null=True, blank=True, verbose_name='그룹 목적')
     # tendency = models.ForeignKey() => 키워드에서 외래키 걸기
-    #블랙 리스트와 그룹 멤버는 monytomanyfield를 통해서 하는게?
-    # member = models.ManyToManyField('User')
-    # blackList = models.ManyToManyField('User')
+    
+    member = models.ManyToManyField(User, related_name='member_group')
+    blackList = models.ManyToManyField(User, blank=True, related_name='blackList_group')
 
     #이미지 업로드 경로 추가 
-    image = models.ImageField(blank=True, upload_to='groups/%Y%m%d', verbose_name='사진')
+    image = models.ImageField(null=True, blank=True, upload_to='groups/%Y%m%d', verbose_name='그룹 대표사진')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- groups/views.py
+++ groups/views.py
@@ -1,11 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from groups.forms import GroupForm
+from .models import Group
+from logins.models import User
 
 # Create your views here.
 def main(request):
-    pass
+    group = Group.objects.first()
+    print(group)
+    return render(request, 'groups/main.html')
 
 def create(request):
-    pass
+    if request.method == 'POST':
+        form = GroupForm(request.POST, request.FILES)
+        if form.is_valid():
+            group = form.save(commit=False)
+            group.head = request.user.username
+            group.code = request.user.username
+            group.save()
+            group.member.add(request.user.id)
+        return redirect('/groups/')
+    else:
+        form = GroupForm()
+        context = {
+            'form' : form
+        }
+        return render(request, template_name='groups/create.html', context=context)
 
 def join(requsest):
+    pass
+
+def detail(request, id):
     pass
