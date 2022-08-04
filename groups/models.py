5a40b47508f10b65ff3ca0e1686bf1a9b96b13fb
doongdang <whtjrgus0428@gmail.com>
2022-08-04 16:03:26 +0900

    feat: groups 모델 세팅

--- groups/models.py
+++ groups/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Group(models.Model):
+    name = models.CharField(max_length=40, verbose_name='그룹명')
+    head = models.CharField(max_length=20, verbose_name='그룹장')
+    code = models.CharField(max_length=30, verbose_name='그룹 코드')
+    introduction = models.TextField(null=True, blank=True, verbose_name='그룹 소개')
+    purpose = models.TextField(null=True, blank=True, verbose_name='그룹 목적')
+    # tendency = models.ForeignKey() => 키워드에서 외래키 걸기
+    #블랙 리스트와 그룹 멤버는 monytomanyfield를 통해서 하는게?
+    # member = models.ManyToManyField('User')
+    # blackList = models.ManyToManyField('User')
+
+    #이미지 업로드 경로 추가 
+    image = models.ImageField(blank=True, upload_to='groups/%Y%m%d', verbose_name="사진")
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    
