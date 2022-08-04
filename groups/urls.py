31a84c53267c7ae31e1fdf7fc49025266bc30d07
doongdang <whtjrgus0428@gmail.com>
2022-08-04 16:20:10 +0900

    feat: main, create, join url 매핑

--- groups/urls.py
+++ groups/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
-app_name = "groups"
+app_name = 'groups'
 
 urlpatterns = [
-    
+    path('', views.main, name='main'),
+    path('create/', views.create, name='create'),
+    path('join/', views.join, name='join'),
+
 ]
