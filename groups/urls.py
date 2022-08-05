25549fb856f255a7561f78fe5497e9d81cefc14b
doongdang <whtjrgus0428@gmail.com>
2022-08-05 14:52:04 +0900

    feat: 그룹 디테일 페이지 url 추가

--- groups/urls.py
+++ groups/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.main, name='main'),
     path('create/', views.create, name='create'),
     path('join/', views.join, name='join'),
+    path('group/<int:id>/', views.detail, name='detail'),
 
 ]
