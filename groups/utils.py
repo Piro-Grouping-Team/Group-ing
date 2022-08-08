a76c51d249a63993fd6fd7a5471910cc9abe5b26
doongdang <whtjrgus0428@gmail.com>
2022-08-09 01:25:59 +0900

    fix: 그룹 코드 함수 수정

--- groups/utils.py
+++ groups/utils.py
@@ -3,8 +3,7 @@ import random
 
 def groupCodeGenerate(username, groupId):
     code = ''
-    nameLength = len(username)
-    randomNames = list(itertools.permutations(username,nameLength))
+    randomNames = list(itertools.permutations(username, 5))
     randomName = random.choice(randomNames)    
     for alphabet in randomName:
         code += format(ord(alphabet), 'x')
