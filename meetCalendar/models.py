8f71adbc80365c05112773d8ad56eb5fcbfc4076
doongdang <whtjrgus0428@gmail.com>
2022-08-12 17:36:19 +0900

    fix: meetCalendar 모델 필드 임시

--- meetCalendar/models.py
+++ meetCalendar/models.py
@@ -1,12 +1,22 @@
+from tkinter import CASCADE
 from django.db import models
+
+from meetings.models import Meetings
+from logins.models import User
 
 # Create your models here.
 
 #당일치기 history 모델
 class meetDay(models.Model):
     #약속 pk
+    meetId = models.ForeignKey(Meetings, on_delete=CASCADE, verbose_name='약속 PK', related_name='meetId')
+
     #user pk
+    userId = models.ForeignKey(User, on_delete=CASCADE, verbose_name='사용자 PK', related_name='userId')
+
     #시작시간
+    startTime = models.CharField
+
     #종료시간
     
     pass
