f0a94d91aa129e71898ec74fc20e78617d9b04b7
doongdang <whtjrgus0428@gmail.com>
2022-08-14 03:35:25 +0900

    Fix: meetDat Field fix

--- meetCalendar/models.py
+++ meetCalendar/models.py
@@ -7,19 +7,49 @@ from logins.models import User
 
 #당일치기 history 모델
 class meetDay(models.Model):
+    TIME_CHOICE = (
+        ('0', '00 ~ 01'),
+        ('1', '01 ~ 02'),
+        ('2', '02 ~ 03'),
+        ('3', '03 ~ 04'),
+        ('4', '04 ~ 05'),
+        ('5', '05 ~ 06'),
+        ('6', '06 ~ 07'),
+        ('7', '07 ~ 08'),
+        ('8', '08 ~ 09'),
+        ('9', '09 ~ 10'),
+        ('10', '10 ~ 11'),
+        ('11', '11 ~ 12'),
+        ('12', '12 ~ 13'),
+        ('13', '13 ~ 14'),
+        ('14', '14 ~ 15'),
+        ('15', '15 ~ 16'),
+        ('16', '16 ~ 17'),
+        ('17', '17 ~ 18'),
+        ('18', '18 ~ 19'),
+        ('19', '19 ~ 20'),
+        ('20', '20 ~ 21'),
+        ('21', '21 ~ 22'),
+        ('22', '22 ~ 23'),
+        ('23', '23 ~ 00'),
+    )
+
+
     #약속 pk
     meetId = models.ForeignKey(Meetings, on_delete=models.CASCADE, verbose_name='약속 PK')
 
     #user pk
     userId = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='사용자 PK')
 
-    #시작시간
-    startTime = models.DateTimeField(verbose_name='시작시간')
+    #가능 날짜
+    validDate = models.DateField()
+
+    #시작 시간
+    startTime = models.CharField(max_length=20,choices=TIME_CHOICE)
 
     #종료시간
-    endTime = models.DateTimeField(verbose_name='종료시간')
+    endTime = models.CharField(max_length=20, choices=TIME_CHOICE)
     
-    pass
 
 
 #여행 history 모델
@@ -32,7 +62,6 @@ class meetTravel(models.Model):
     startDate = models.DateTimeField(verbose_name='시작 날짜')
     #종료 날짜
     endDate = models.DateTimeField(verbose_name='종료 날짜')
-    pass
 
 
 #당일치기 통계 모델
@@ -51,7 +80,6 @@ class meetDayInfo(models.Model):
         size=24,
         )
     #array필드 (인덱스가 시간인)
-    pass
 
 
 #여행 통계 모델
@@ -68,5 +96,3 @@ class meetTravelInfo(models.Model):
     meetUsers = models.ManyToManyField(User, related_name='meetUser')
 
     #array필드 ()
-  
-    pass
