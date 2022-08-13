a090293a0be262f895449bcc8c7ba34e349b5d5c
doongdang <whtjrgus0428@gmail.com>
2022-08-14 03:35:08 +0900

    Feat: Create meetDayForm

--- meetCalendar/forms.py
+++ meetCalendar/forms.py
@@ -7,3 +7,7 @@ class meetDayForm(forms.ModelForm):
     class Meta:
         model = meetDay
         fields = ['validDate', 'startTime', 'endTime']
+        widgets = {
+            'validDate' : forms.DateInput(format=('%m/%d/%Y'),attrs={'class':'form-control', 'placeholder':'Select a date', 'type':'date'}),
+    
+        }
