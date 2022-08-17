fd63e689d1b6236fa45f1aaddb4a6494e1aec89e
hamo-o <yheel369@gmail.com>
2022-08-17 10:03:10 +0900

    feat: 캘린더 일 단위 색칠

--- meetings/views.py
+++ meetings/views.py
@@ -30,8 +30,9 @@ def create(request,id):
         startDate = request.POST['startDate']
         endDate = request.POST['endDate']
         meetPurpose = request.POST['meetPurpose']
+        meetType = request.POST['meetType']
 
-        meetings = Meetings(meetHead=meetHead,meetName=meetName,meetGroupId=meetGroupId, meetPlace=meetPlace, meetStart=startDate, meetEnd=endDate, meetPurpose=meetPurpose)
+        meetings = Meetings(meetHead=meetHead,meetName=meetName,meetGroupId=meetGroupId, meetPlace=meetPlace, meetStart=startDate, meetEnd=endDate, meetPurpose=meetPurpose, meetType=meetType)
         meetings.save()
 
         return redirect('meetings:detail',id,meetings.id)
