fb28d0d2bd4b8a4828bda96cc3600b93ce16ef4e
Ukj0ng <xb253q@gmail.com>
2022-08-19 15:13:05 +0900

    feat:비밀번호 수정

--- logins/views.py
+++ logins/views.py
@@ -55,6 +55,7 @@ class SignUp(View):
     
     def post(self, request):
         form = forms.SignUpForm(request.POST, request.FILES)
+        print('asdf')
         if form.is_valid():
             user = form.save(commit=False)
             user.save()
@@ -71,7 +72,7 @@ class SignUp(View):
             email = EmailMessage(mail_subject, message, to=[to_email])
             email.send()
             return redirect('/')
-        print(form.error_messages)
+        print(form.errors)
 
 def activate(request, uidb64, token):
     try:
@@ -236,51 +237,22 @@ def authPwResetView(request):
     
     return render(request, 'logins/passwordReset.html', {'form':resetPwForm})
 
-# def findPW(request):
-#     errorMessage=''
-#     context = {'errorMessage' : errorMessage}
-#     if request.method == 'POST':
-#         username = request.POST['username']
-#         name = request.POST['name']
-#         email = request.POST['email']
-#         if User.objects.filter(username=username, name = name, email = email).exists():
-#             user = User.objects.get(username=username, name=name, email=email)
-#             current_site = get_current_site(request)
-#             print(urlsafe_base64_encode(force_bytes(user.id)).encode().decode())
-#             print(account_activation_token.make_token(user))
-#             message = render_to_string('logins/findPW_email.html', {
-#                 'user': user,
-#                 'domain': current_site.domain,
-#                 'id': urlsafe_base64_encode(force_bytes(user.id)).encode().decode(),
-#                 'token': account_activation_token.make_token(user),
-#             })
-#             mail_subject = 'Find your PW'
-#             to_email = user.email
-#             email = EmailMessage(mail_subject, message, to=[to_email])
-#             email.send()
-#             return redirect('logins:login')
-#         else:
-#             errorMessage='일치하는 정보가 없습니다.'
-#             return render(request, template_name='logins/findPW.html', context=context)
-#     return render(request, template_name='logins/findPW.html', context=context)
-
-# def changePW(request, uidb64, token):
-#     try:
-#         uid = force_str(urlsafe_base64_decode(uidb64))
-#         user = User.objects.get(pk=uid)
-#     except(TypeError, ValueError, OverflowError, User.DoesNotExist):
-#         user = None
-#     if user is not None and account_activation_token.check_token(user, token):
-#         if request.method == 'POST':
-#             form = forms.CustomPasswordChangeForm(user, request.POST)
-#             if form.is_valid():
-#                 user = form.save()
-#                 update_session_auth_hash(request, user)
-#                 messages.success(request, '비밀번호가 성공적으로 변경되었습니다!')
-#                 return redirect('/')
-#             else:
-#                 messages.error(request, '오류를 수정해주세요')
-#         else:
-#             form = forms.CustomPasswordChangeForm(request.user)
-#             print(form)
-#         return render(request, 'logins/changePW.html', {'form': form})
+def changePW(request, id):
+    try:
+        user = User.objects.get(pk=id)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user:
+        if request.method == 'POST':
+            form = forms.CustomPasswordChangeForm(user, request.POST)
+            
+            if form.is_valid():
+                user = form.save()
+                update_session_auth_hash(request, user)
+                messages.success(request, '비밀번호가 성공적으로 변경되었습니다!')
+                return redirect('/')
+            else:
+                messages.error(request, '오류를 수정해주세요')
+        else:
+            form = forms.CustomPasswordChangeForm(request.user)
+        return render(request, 'logins/changePW.html', {'form': form})
