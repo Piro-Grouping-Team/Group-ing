8cba06c9e914f88c4cae6811d9d0adbbb764ee64
seongha <11pi885@gmail.com>
2022-08-22 19:42:04 +0900

    fix: 그룹 멤버에서 삭제 버튼 동작 수정

--- static/js/groups/members.js
+++ static/js/groups/members.js
@@ -1,15 +1,28 @@
 const modal = document.querySelector(".modal");
-const newBtn = document.querySelector(".new-btn");
+const newBtn = document.querySelectorAll(".new-btn");
 let flag = 1;
 
-newBtn.addEventListener("click", (event) => {
+newBtn.forEach((btn) => { btn.addEventListener("click", (event) => {
   if (flag == 0) {
     modal.style.display = "none";
-    newBtn.innerHTML = `<i class="fa-solid fa-circle-plus fa-lg"></i>`;
+    btn.innerHTML = `<i class="fa-solid fa-circle-plus fa-lg"></i>`;
     flag = 1;
   } else {
     modal.style.display = "flex";
-    newBtn.innerHTML = `<i class="fa-solid fa-circle-xmark fa-lg"></i>`;
+    btn.innerHTML = `<i class="fa-solid fa-circle-xmark fa-lg"></i>`;
     flag = 0;
   }
+})
 });
+
+// newBtn.addEventListener("click", (event) => {
+//   if (flag == 0) {
+//     modal.style.display = "none";
+//     newBtn.innerHTML = `<i class="fa-solid fa-circle-plus fa-lg"></i>`;
+//     flag = 1;
+//   } else {
+//     modal.style.display = "flex";
+//     newBtn.innerHTML = `<i class="fa-solid fa-circle-xmark fa-lg"></i>`;
+//     flag = 0;
+//   }
+// });
